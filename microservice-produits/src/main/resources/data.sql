59ef488a8355ed743f5a046be196540b0a915362
ayoubhamaoui <ayoub10hamaoui@gmail.com>
2020-01-09 13:42:33 +0100

    first commit

---
+++ microservice-produits/src/main/resources/data.sql
@@ -0,0 +1,16 @@
+INSERT INTO product (ID , TITRE ,DESCRIPTION ,IMAGE ,PRIX ) VALUES (0, 'Bougie fonctionnant au feu', 'bougie qui fonctionne comme une ampoule mais sans électricité !', 'https://www.elyboo.fr/67-large_default/bougie-elyboo-vanille.jpg', 22.0);
+
+INSERT INTO product (ID , TITRE ,DESCRIPTION ,IMAGE ,PRIX ) VALUES (1, 'Chaise pour s''assoire', 'Chaise rare avec non pas 1 ni 2 mais 3 pieds', 'http://www.a2bprovence.fr/images/a2bprovencefr/717-chaises-de-cuisine-tous-modeles-confortables-eventuellement-en-couleur-3251.jpg', 95.0);
+
+INSERT INTO product (ID , TITRE ,DESCRIPTION ,IMAGE ,PRIX ) VALUES (2, 'Cheval pour nains', 'Ce cheval ne portera certainement pas blanche neige, mais sans problème les nains', 'http://static.galerieslafayette.com/media/3004067/300406709703/G_300406709703_278_VFP_1.jpg', 360.0);
+
+INSERT INTO product (ID , TITRE ,DESCRIPTION ,IMAGE ,PRIX ) VALUES (3, 'Coq of steel, le superman des volailles', 'Ne passe pas au four', 'https://www.bencotex.com/wp-content/uploads/2016/03/295.jpg', 620.0);
+
+INSERT INTO product (ID , TITRE ,DESCRIPTION ,IMAGE ,PRIX ) VALUES (4, 'Flacon à frotter avec un génie dedans', 'Vous donne droit à l''équivalent de 3/0 voeux', 'https://d2oj7i4l5tmbkc.cloudfront.net/cache/media/2018/10/1chx015s0_23024.jpg/f56d478c7cc1e393b9802925860335f5.jpg', 1200.0);
+
+INSERT INTO product (ID , TITRE ,DESCRIPTION ,IMAGE ,PRIX ) VALUES (5, 'Horloge quantique', 'Donne l''heure, les minutes et même les secondes. Ne fait pas de café', 'https://www.lacorbeille.fr/14826-large_default/horloge-clork.jpg', 180.0);
+
+INSERT INTO product (ID , TITRE ,DESCRIPTION ,IMAGE ,PRIX ) VALUES (6, 'Table d''opération pour Hamsters', 'Pour réaliser vos opérations chirugicales sur votre Hamster!', 'https://www.knoll.com/static_resources/images/products/catalog/eco/parts/642TR/642TR-KC_KC_FZ.jpg', 210.0);
+
+INSERT INTO product (ID , TITRE ,DESCRIPTION ,IMAGE ,PRIX ) VALUES (7 , 'Vase ayant appartenu a Zeus', 'Risque de choc électrique', 'https://oasisfloralproducts.com/wp-content/uploads/2018/04/E905_HR.jpg', 730.0);
+
